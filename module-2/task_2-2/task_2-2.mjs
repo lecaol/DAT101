403cf8d742364dd132bd837e0adbebfd565b477a
lecaol <leacarineolsen@gmail.com>
2025-12-03 22:12:08 +0100

    Change 1

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,12 +4,21 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut(2+3*2-4*6);
+const original = 2 + 3 * 2 - 4 * 6;
+const modified= 2+(3 * (2 - 4) * 6);
+printOut("Original expression: 2+3*2-4*6= " + original);
+printOut("Modified expression: 2+(3*(2-4)*6)= " + modified);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const meters = 25;
+const cm = 34;
+const mm = 25.4; 
+let OneInch = mm;
+let TotalCm = (meters * 100) + cm;
+let TotalInches = (TotalCm * 10) / OneInch;
+printOut("25 meter og 34 cm er tilsvarende" + TotalInches.toFixed(2) + " inches");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
